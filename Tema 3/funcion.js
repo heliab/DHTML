c9e5083b4f9746452cde97e567509673b1fd1e63
heliab <lhaniel@outlook.com>
2015-12-20 10:21:01 -0600

    getElementById

---
+++ Tema 3/funcion.js
@@ -0,0 +1,10 @@
+function cambiarColor()
+{
+	var tit=document.getElementById('titulo');
+	tit.style.color='#007fff';
+}
+function cambiarTamanoFuente()
+{
+  var tit=document.getElementById('titulo');
+  tit.style.fontSize="xx-large";
+}
